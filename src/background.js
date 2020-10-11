632f615644704240d22c999c798b754f24263677
Duarte David <deltaduartedavid@gmail.com>
2020-10-11 16:16:18 +0200

    Initial commit

---
+++ src/background.js
@@ -0,0 +1,18 @@
+import Triangulr from 'triangulr'
+
+function color(path) {
+	const colors = [
+		'#87CEEB',
+		'#FEBFD2',
+		'#BFA7D2',
+		'#93B9D6',
+		//'#eeeeee',
+	]
+	console.log(path)
+	return colors[(path.y + Math.floor(Math.random() * 2) - 1 + colors.length) % colors.length] + Math.floor(50 + Math.random() * 205).toString(16).padStart(2, '0')
+}
+
+const svg = new Triangulr(600, 500, 40, 10, color)
+
+svg.classList.add("background")
+document.body.prepend(svg)
