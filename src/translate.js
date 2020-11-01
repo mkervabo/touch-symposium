82a5f593f02e14df5a01a06f3145f03adc02da91
mkervabo <mkervabo@student.42.fr>
2020-11-01 22:33:32 +0100

    add speakers

--- src/translate.js
+++ src/translate.js
@@ -6,14 +6,16 @@ const fr = {
   what_title: 'Computer Programming, Humanities, Music',
   what_body: `<3 TØUCH > est un symposium mixant art-technologie-philosophie et dont la chaire se situe dans le Paris cyberpunk.
 <3 TØUCH > est un mélange de musique électro, d'esthétique de haut niveau, de vibe féminine, de science avancée, de programmation éthique et d' hacktivisme sophistiqué.
-<3 TØUCH > symposium will mix music, talks and digital art to set the tracks for interdisciplinary critical and creative debates in order to educate, inform and prevent`,
+<3 TØUCH > symposium mêlera musique, conférences et art numérique pour ouvrir la voie à des débats interdisciplinaires critiques et créatifs afin d'éduquer, d'informer et de prévenir`,
   where_title: 'WHERE ?',
   where_body: 'The Holodeck at #42born2code - Recording studio for interdisciplinary creative debates on your screen',
-  when_title: 'December 3rd',
-  how_title: 'Mission statement:',
+  when_title: '3 Décembre',
+  how_title: 'Mission:',
   how_body: `Notre monde numérique et IoT est une société d'échantillonnage.
 Boucler des fragments de données ici et là pour alimenter les technologies informatisées.
-Le 3 décembre 2020, nous discuterons pourquoi et comment programmer des codes éthiques et créatifs.`,
+Le 3 décembre 2020, nous discuterons pourquoi et comment programmer des codes éthiques et créatifs.
+Les artistes français et allemands issus de la musique électronique ont saisi avant l'heure nos enjeux sociétaux. 
+Depuis les années 70, Kraftwerk, Daft Punk, Miss Kittin & The Hacker, Chicks on Speed, Ellen Allien ont observé la fabrication du monde technologique dans lequel nous vivons. Les hommes-machines, les robots, la protection de la vie privée, les données numériques et l'intelligence artificielle les ont inspiré pour informer le grand public sur ces sujets de manière créative.`,
 };
 
 function translate(lang) {
